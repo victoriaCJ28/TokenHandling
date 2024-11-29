303c397fa923d8892f9f1396b69d36b21f4d5074
Victoria Castro <victoria.akicastro92@gmail.com>
2024-11-28 23:01:15 -0600

    Modificando estructura

--- primeng-app-login/src/app/app.routes.ts
+++ primeng-app-login/src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { HomeComponent } from './pages/home/home.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+
 
 export const routes: Routes = [
    {path: 'login', component: LoginComponent},
    {path: 'dashboard', component: DashboardComponent},
-   {path: 'home', component: HomeComponent},
-   {path: '', redirectTo: 'home', pathMatch: 'full'},
+   {path: 'layout', component: LayoutComponent},
+   {path: '', redirectTo: 'login', pathMatch: 'full'},
    
 ];
